d8f0546a8e0c816e81d4b9d200f8956f369fb82b
kariastnor <karin.noresten@live.com>
2022-05-11 11:25:29 +0200

    Updates

---
+++ precache-manifest.22c7dbb92b40ae46419c25a4ef71b8b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da0afca3298cbd480e08049c36a7ae0",
+    "url": "/grocery-list/index.html"
+  },
+  {
+    "revision": "08c1e68b501b7901b5f8",
+    "url": "/grocery-list/static/css/main.9021244c.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/grocery-list/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grocery-list/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c1e68b501b7901b5f8",
+    "url": "/grocery-list/static/js/main.10aa1d7f.chunk.js"
+  },
+  {
+    "revision": "442f1a7391b3cc56a3ca",
+    "url": "/grocery-list/static/js/runtime-main.a51afe8e.js"
+  }
+]);
